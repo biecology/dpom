53558183962a57043ff1f3c101d52cf496143024
DPOM <48582142+biecology@users.noreply.github.com>
2020-07-16 11:01:46 +0800

    Add files via upload

---
+++ TableSupport.java
@@ -0,0 +1,29 @@
+
+import com.common.constant.Constants;
+import com.common.utils.ServletUtils;
+
+/**
+ * 
+ * 
+ * 
+ */
+public class TableSupport
+{
+    /**
+     * 
+     */
+    public static PageDomain getPageDomain()
+    {
+        PageDomain pageDomain = new PageDomain();
+        pageDomain.setPageNum(ServletUtils.getParameterToInt(Constants.PAGE_NUM));
+        pageDomain.setPageSize(ServletUtils.getParameterToInt(Constants.PAGE_SIZE));
+        pageDomain.setOrderByColumn(ServletUtils.getParameter(Constants.ORDER_BY_COLUMN));
+        pageDomain.setIsAsc(ServletUtils.getParameter(Constants.IS_ASC));
+        return pageDomain;
+    }
+
+    public static PageDomain buildPageRequest()
+    {
+        return getPageDomain();
+    }
+}
